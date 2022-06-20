03f4b0a09a2eeaf9b81508f9af05beaee7e661f5
liuqh <lqhday@gmail.com>
2022-06-20 18:48:47 +0800

    优化脚本

--- dev.sh
+++ dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd C:\\Users\\qingh\\AppData\\Local\\shopify\\sv
-rm ngrok.log
-rm ngrok.pid
-cd E:\\code\\vite\\result
+rm C:\\Users\\qingh\\AppData\\Local\\shopify\\sv\\ngrok.log > /dev/null 2>&1
+rm C:\\Users\\qingh\\AppData\\Local\\shopify\\sv\\ngrok.pid > /dev/null 2>&1
+
 shopify app serve
